e40425d09ab9103c46f3d5b46be0409f7d0c58a8
Hossein <hossein.ghermezcheshme@gmail.com>
2023-11-14 10:09:34 +0330

    It doesnt work.

--- main.py
+++ main.py
@@ -33,6 +33,174 @@ class Game:
                     print(self.mapGame.array[i][j], end=" ")
             print("")
 
+    def doesEatAllFoods(self):
+        for i in range(self.mapGame.height):
+            for j in range(self.mapGame.width):
+                if self.mapGame.array[i][j] == "+":
+                    return False
+        return True
+
+    def finishGame(self):
+        if (self.pacman.x == self.ghost1.x and self.pacman.y == self.ghost1.y) or (
+                self.pacman.x == self.ghost2.x and self.pacman.y == self.ghost2.y):
+            return -1
+        elif self.doesEatAllFoods():
+            return 1
+        else:
+            return 0
+
+    def ghostMove(self, gameMapArray, gh):
+        direction = {0: "U", 1: "R", 2: "D", 3: "L"}
+        while True:
+            r = random.randint(0, 3)
+            if r == 0 and self.mapGame.array[gh.x - 1][gh.y] != "#":
+                return gh.x - 1, gh.y
+            if r == 1 and self.mapGame.array[gh.x][gh.y + 1] != "#":
+                return gh.x, gh.y + 1
+            if r == 2 and self.mapGame.array[gh.x + 1][gh.y] != "#":
+                return gh.x + 1, gh.y
+            if r == 3 and self.mapGame.array[gh.x][gh.y - 1] != "#":
+                return gh.x, gh.y - 1
+
+    def validMove(self, x, y, move):
+        if move == "U":
+            x -= 1
+        if move == "R":
+            y += 1
+        if move == "D":
+            x += 1
+        if move == "L":
+            y -= 1
+
+        if self.mapGame.array[x][y] != "#":
+            return x, y
+        else:
+            return -1
+
+    def bestMove(self, target):
+        move = "D"
+        bestScore = float('-inf')
+        for i in ["U", "R", "D", "L"]:
+            if self.validMove(self.pacman.x, self.pacman.y, i) != -1:
+                pacman_future_x, pacman_future_y = self.validMove(self.pacman.x, self.pacman.y, i)
+                pacman_now_x, pacman_now_y = self.pacman.x, self.pacman.y
+                self.pacman.x, self.pacman.y = pacman_future_x, pacman_future_y
+
+                food = self.mapGame.array[pacman_future_x][pacman_future_y]
+
+                score = self.minimax(0, target, 0)
+
+                self.pacman.x, self.pacman.y = pacman_now_x, pacman_now_y
+                self.mapGame.array[pacman_future_x][pacman_future_y] = food
+
+                if score > bestScore:
+                    bestScore = score
+                    move = i
+
+        x, y = self.validMove(self.pacman.x, self.pacman.y, move)
+        self.mapGame.array[x][y] = " "
+        return self.validMove(self.pacman.x, self.pacman.y, move)
+
+    def minimax(self, currentDepth, targetDepth, isMaximizing):
+        if currentDepth == targetDepth:
+            self.score = self.scoreFunction()
+            return self.score
+
+        if isMaximizing == 0:
+            bestScore = float('-inf')
+            for i in ["U", "R", "D", "L"]:
+                if self.validMove(self.pacman.x, self.pacman.y, i) != -1:
+                    pacman_future_x, pacman_future_y = self.validMove(self.pacman.x, self.pacman.y, i)
+                    pacman_now_x, pacman_now_y = self.pacman.x, self.pacman.y
+
+                    food = self.mapGame.array[pacman_future_x][pacman_future_y]
+
+                    self.pacman.x, self.pacman.y = pacman_future_x, pacman_future_y
+
+                    score = self.minimax(currentDepth, targetDepth, 1)
+
+                    self.mapGame.array[pacman_future_x][pacman_future_y] = food
+                    self.pacman.x, self.pacman.y = pacman_now_x, pacman_now_y
+
+                    bestScore = max(bestScore, score)
+            return bestScore
+        elif isMaximizing == 1:
+            bestScore = float('inf')
+            for i in ["U", "R", "D", "L"]:
+                if self.validMove(self.ghost1.x, self.ghost1.y, i) != -1:
+                    ghost1_future_x, ghost1_future_y = self.validMove(self.ghost1.x, self.ghost1.y, i)
+                    ghost1_now_x, ghost1_now_y = self.ghost1.x, self.ghost1.y
+
+                    self.ghost1.x, self.ghost1.y = ghost1_future_x, ghost1_future_y
+
+                    score = self.minimax(currentDepth, targetDepth, 2)
+
+                    self.ghost1.x, self.ghost1.y = ghost1_now_x, ghost1_now_y
+
+                    bestScore = min(bestScore, score)
+            return bestScore
+        elif isMaximizing == 2:
+            bestScore = float('inf')
+            for i in ["U", "R", "D", "L"]:
+                if self.validMove(self.ghost2.x, self.ghost2.y, i) != -1:
+                    ghost2_future_x, ghost2_future_y = self.validMove(self.ghost2.x, self.ghost2.y, i)
+                    ghost2_now_x, ghost2_now_y = self.ghost2.x, self.ghost2.y
+
+                    self.ghost2.x, self.ghost2.y = ghost2_future_x, ghost2_future_y
+
+                    score = self.minimax(currentDepth + 1, targetDepth, 0)
+
+                    self.ghost2.x, self.ghost2.y = ghost2_now_x, ghost2_now_y
+
+                    bestScore = min(score, bestScore)
+            return bestScore
+
+    def scoreFunction(self):
+        fromGhost1 = math.sqrt(
+            (self.pacman.x - self.ghost1.x) ** 2 + (self.pacman.y - self.ghost1.y) ** 2)
+        fromGhost2 = math.sqrt(
+            (self.pacman.x - self.ghost2.x) ** 2 + (self.pacman.y - self.ghost2.y) ** 2)
+
+        closest = float('inf')
+        A = math.sqrt((self.mapGame.height - 2) ** 2 + (self.mapGame.width - 2) ** 2)
+        for i in range(self.mapGame.height):
+            for j in range(self.mapGame.width):
+                if self.mapGame.array[i][j] == "+":
+                    fromFood = math.sqrt(((self.pacman.x - i) ** 2) + (self.pacman.y - j) ** 2)
+                    if fromFood < closest:
+                        closest = fromFood
+
+        self.score += ((A - closest) / A) * 9
+
+        if min(fromGhost1, fromGhost2) < 3:
+            self.score *= -1
+
+        return self.score
+
+    def startGame(self, target, turn):
+        i = 0
+        while True:
+            i += 1
+            self.printGame()
+            if self.finishGame() == -1:
+                print("GAME OVER")
+                break
+            elif self.finishGame() == 1:
+                print("YOU WIN")
+                break
+
+            if turn == 0:
+                self.pacman.x, self.pacman.y = self.bestMove(target)
+                turn = 1
+            if turn == 1:
+                self.ghost1.x, self.ghost1.y = self.ghostMove(self.mapGame.array, self.ghost1)
+                turn = 2
+            if turn == 2:
+                self.ghost2.x, self.ghost2.y = self.ghostMove(self.mapGame.array, self.ghost2)
+                turn = 0
+            sleep(0.01)
+            os.system("cls")
+
 
 class Pacman:
     def __init__(self, locX, locY):
@@ -146,192 +314,7 @@ mapGame.fillMap()
 
 game = Game(mapGame, pacman, ghost1, ghost2, 0, 0)
 
-
 # </editor-fold>
 
 
-def doesEatAllFoods(gameTemp):
-    for i in range(gameTemp.mapGame.height):
-        for j in range(gameTemp.mapGame.width):
-            if gameTemp.mapGame.array[i][j] == "+":
-                return False
-    return True
-
-
-def finishGame(gameTemp):
-    if (gameTemp.pacman.x == gameTemp.ghost1.x and gameTemp.pacman.y == gameTemp.ghost1.y) or (
-            gameTemp.pacman.x == gameTemp.ghost2.x and gameTemp.pacman.y == gameTemp.ghost2.y):
-        return -1
-    elif doesEatAllFoods(gameTemp):
-        return 1
-    else:
-        return 0
-
-
-def ghostMove(gameMapArray, gh):
-    direction = {0: "U", 1: "R", 2: "D", 3: "L"}
-    while True:
-        r = random.randint(0, 3)
-        if r == 0 and gameMapArray[gh.x - 1][gh.y] != "#":
-            return gh.x - 1, gh.y
-        if r == 1 and gameMapArray[gh.x][gh.y + 1] != "#":
-            return gh.x, gh.y + 1
-        if r == 2 and gameMapArray[gh.x + 1][gh.y] != "#":
-            return gh.x + 1, gh.y
-        if r == 3 and gameMapArray[gh.x][gh.y - 1] != "#":
-            return gh.x, gh.y - 1
-
-
-def validMove(gameTemp, x, y, move):
-    if move == "U":
-        x -= 1
-    if move == "R":
-        y += 1
-    if move == "D":
-        x += 1
-    if move == "L":
-        y -= 1
-
-    if gameTemp.mapGame.array[x][y] != "#":
-        return x, y
-    else:
-        return -1
-
-
-def bestMove(gameTemp, target):
-    move = "D"
-    bestScore = float('-inf')
-    for i in ["U", "R", "D", "L"]:
-        if validMove(gameTemp, gameTemp.pacman.x, gameTemp.pacman.y, i) != -1:
-            pacman_future_x, pacman_future_y = validMove(gameTemp, gameTemp.pacman.x, gameTemp.pacman.y, i)
-            pacman_now_x, pacman_now_y = gameTemp.pacman.x, gameTemp.pacman.y
-            gameTemp.pacman.x, gameTemp.pacman.y = pacman_future_x, pacman_future_y
-
-            food = gameTemp.mapGame.array[pacman_future_x][pacman_future_y]
-
-            # print("Target: ", target)
-            score = minimax(gameTemp, 0, target, 0)
-
-            gameTemp.pacman.x, gameTemp.pacman.y = pacman_now_x, pacman_now_y
-            gameTemp.mapGame.array[pacman_future_x][pacman_future_y] = food
-
-            if score > bestScore:
-                bestScore = score
-                move = i
-            # print("score: ", i, bestScore)
-
-    x, y = validMove(gameTemp, gameTemp.pacman.x, gameTemp.pacman.y, move)
-    gameTemp.mapGame.array[x][y] = " "
-    return validMove(gameTemp, gameTemp.pacman.x, gameTemp.pacman.y, move)
-
-
-def scoreFunction(gameTemp):
-    fromGhost1 = math.sqrt((gameTemp.pacman.x - gameTemp.ghost1.x) ** 2 + (gameTemp.pacman.y - gameTemp.ghost1.y) ** 2)
-    fromGhost2 = math.sqrt((gameTemp.pacman.x - gameTemp.ghost2.x) ** 2 + (gameTemp.pacman.y - gameTemp.ghost2.y) ** 2)
-
-    closest = float('inf')
-    A = math.sqrt((gameTemp.mapGame.height - 2) ** 2 + (gameTemp.mapGame.width - 2) ** 2)
-    for i in range(gameTemp.mapGame.height):
-        for j in range(gameTemp.mapGame.width):
-            if gameTemp.mapGame.array[i][j] == "+":
-                fromFood = math.sqrt(((gameTemp.pacman.x - i) ** 2) + (gameTemp.pacman.y - j) ** 2)
-                # print("from food", fromFood)
-                if fromFood < closest:
-                    closest = fromFood
-
-    # print("Closet: ", closest)
-    gameTemp.score = ((A - closest) / A) * 9
-    # print("sssssssssssssssss", gameTemp.score)
-
-    if min(fromGhost1, fromGhost2) < 3:
-        gameTemp.score *= -1
-
-    return gameTemp.score
-
-    # if min(fromGhost2, fromGhost1) < 3:
-    #     return min(fromGhost2, fromGhost1)
-    # else:
-    #     return gameTemp.score
-
-
-def minimax(gameTemp, currentDepth, targetDepth, isMaximizing):
-    if currentDepth == targetDepth:
-        gameTemp.score = scoreFunction(gameTemp)
-        return gameTemp.score
-
-    if isMaximizing == 0:
-        bestScore = float('-inf')
-        for i in ["U", "R", "D", "L"]:
-            if validMove(gameTemp, gameTemp.pacman.x, gameTemp.pacman.y, i) != -1:
-                pacman_future_x, pacman_future_y = validMove(gameTemp, gameTemp.pacman.x, gameTemp.pacman.y, i)
-                pacman_now_x, pacman_now_y = gameTemp.ghost1.x, gameTemp.pacman.y
-
-                food = gameTemp.mapGame.array[pacman_future_x][pacman_future_y]
-
-                gameTemp.pacman.x, gameTemp.pacman.y = pacman_future_x, pacman_future_y
-
-                score = minimax(gameTemp, currentDepth, targetDepth, 1)
-
-                gameTemp.mapGame.array[pacman_future_x][pacman_future_y] = food
-                gameTemp.pacman.x, gameTemp.pacman.y = pacman_now_x, pacman_now_y
-
-                bestScore = max(bestScore, score)
-        return bestScore
-    if isMaximizing == 1:
-        bestScore = float('inf')
-        for i in ["U", "R", "D", "L"]:
-            if validMove(gameTemp, gameTemp.ghost1.x, gameTemp.ghost1.y, i) != -1:
-                ghost1_future_x, ghost1_future_y = validMove(gameTemp, gameTemp.ghost1.x, gameTemp.ghost1.y, i)
-                ghost1_now_x, ghost1_now_y = gameTemp.ghost1.x, gameTemp.ghost1.y
-
-                gameTemp.ghost1.x, gameTemp.ghost1.y = ghost1_future_x, ghost1_future_y
-
-                score = minimax(gameTemp, currentDepth, targetDepth, 2)
-
-                gameTemp.ghost1.x, gameTemp.ghost1.y = ghost1_now_x, ghost1_now_y
-
-                bestScore = min(bestScore, score)
-        return bestScore
-    if isMaximizing == 2:
-        bestScore = float('inf')
-        for i in ["U", "R", "D", "L"]:
-            if validMove(gameTemp, gameTemp.ghost2.x, gameTemp.ghost2.y, i) != -1:
-                ghost2_future_x, ghost2_future_y = validMove(gameTemp, gameTemp.ghost2.x, gameTemp.ghost2.y, i)
-                ghost2_now_x, ghost2_now_y = gameTemp.ghost2.x, gameTemp.ghost2.y
-
-                gameTemp.ghost2.x, gameTemp.ghost2.y = ghost2_future_x, ghost2_future_y
-
-                score = minimax(gameTemp, currentDepth + 1, targetDepth, 0)
-
-                gameTemp.ghost2.x, gameTemp.ghost2.y = ghost2_now_x, ghost2_now_y
-
-                gameTemp.score = min(score, bestScore)
-        return bestScore
-
-
-def startGame(gameTemp, target, turn):
-    i = 0
-    while True:
-        i += 1
-        gameTemp.printGame()
-        if finishGame(gameTemp) == -1:
-            print("GAME OVER")
-            break
-        elif finishGame(gameTemp) == 1:
-            print("YOU WIN")
-            break
-
-        if turn == 0:
-            gameTemp.pacman.x, gameTemp.pacman.y = bestMove(gameTemp, target)
-            turn = 1
-        if turn == 1:
-            gameTemp.ghost1.x, gameTemp.ghost1.y = ghostMove(gameTemp.mapGame.array, gameTemp.ghost1)
-            turn = 2
-        if turn == 2:
-            gameTemp.ghost2.x, gameTemp.ghost2.y = ghostMove(gameTemp.mapGame.array, gameTemp.ghost2)
-            turn = 0
-        sleep(0.05)
-        os.system("cls")
-
-
-startGame(game, 0, 0)
+game.startGame(1, 0)
